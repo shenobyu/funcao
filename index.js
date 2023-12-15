f7b407da0c632d2ab221f003254b690cc8e2df0a
shenobyu <shenobyu@gmail.com>
2023-12-15 10:51:14 -0300

    cara esse foi dificil man, eu fiquei perdido em criar repositorio no git, esse git e github e muito dificil pra mim

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("a")
+function cs(vitoria, derrota) {
+    let v = vitoria - derrota;
+    let nome = "shaolin matador de porco"
+    const r = "O Herói " + nome + " tem saldo de " + v + " vitórias e está no nível de "
+  
+if (v < 10){
+    console.log(r + "Ferro")
+}else if(v >= 11 & v <= 20){
+    console.log(r + "Bronze")
+}else if(v >= 21 & v <= 50){
+    console.log(r + "Prata")
+}else if(v >= 51 & v <= 80){
+    console.log(r + "Ouro")
+}else if(v >= 81 & v <= 90){
+    console.log(r + "Diamante")
+}else if(v >= 91 & v <= 100){
+    console.log(r + "lendário")
+}else{
+    console.log(r + "Imortal")
+}
+
+}
+  
+cs(35,5);
+
+
